ba2d74589a0293fcdcbb118cf6e7b9d3ee25945a
Julia Kreutzer <juliakreutzer1@aol.com>
2019-03-11 18:08:36 +0100

    types for model

--- joeynmt/model.py
+++ joeynmt/model.py
@@ -20,9 +20,202 @@ from joeynmt.vocabulary import Vocabulary
 from joeynmt.batch import Batch
 
 
+class Model(nn.Module):
+    """
+    Base Model class
+    """
+
+    def __init__(self,
+                 encoder: Encoder,
+                 decoder: Decoder,
+                 src_embed: Embeddings,
+                 trg_embed: Embeddings,
+                 src_vocab: Vocabulary,
+                 trg_vocab: Vocabulary) -> None:
+        """
+        Create a new encoder-decoder model
+
+        :param encoder: encoder
+        :param decoder: decoder
+        :param src_embed: source embedding
+        :param trg_embed: target embedding
+        :param src_vocab: source vocabulary
+        :param trg_vocab: target vocabulary
+        """
+        super(Model, self).__init__()
+
+        self.src_embed = src_embed
+        self.trg_embed = trg_embed
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_vocab = src_vocab
+        self.trg_vocab = trg_vocab
+        self.bos_index = self.trg_vocab.stoi[BOS_TOKEN]
+        self.pad_index = self.trg_vocab.stoi[PAD_TOKEN]
+        self.eos_index = self.trg_vocab.stoi[EOS_TOKEN]
+
+    #pylint: disable=arguments-differ
+    def forward(self, src: Tensor, trg_input: Tensor, src_mask: Tensor,
+                src_lengths: Tensor) -> (Tensor, Tensor, Tensor, Tensor):
+        """
+        Take in and process masked src and target sequences.
+        Use the encoder hidden state to initialize the decoder
+        The encoder outputs are used for attention
+
+        :param src: source input
+        :param trg_input: target input
+        :param src_mask: source mask
+        :param src_lengths: length of source inputs
+        :return: decoder outputs
+        """
+        encoder_output, encoder_hidden = self.encode(src=src,
+                                                     src_length=src_lengths,
+                                                     src_mask=src_mask)
+        unrol_steps = trg_input.size(1)
+        return self.decode(encoder_output=encoder_output,
+                           encoder_hidden=encoder_hidden,
+                           src_mask=src_mask, trg_input=trg_input,
+                           unrol_steps=unrol_steps)
+
+    def encode(self, src: Tensor, src_length: Tensor, src_mask: Tensor) \
+            -> (Tensor, Tensor):
+        """
+        Encodes the source sentence.
+        TODO adapt to transformer
+
+        :param src:
+        :param src_length:
+        :param src_mask:
+        :return: encoder outputs (output, hidden_concat)
+        """
+        return self.encoder(self.src_embed(src), src_length, src_mask)
+
+    def decode(self, encoder_output: Tensor, encoder_hidden: Tensor,
+               src_mask: Tensor, trg_input: Tensor,
+               unrol_steps: int, decoder_hidden: Tensor = None) \
+            -> (Tensor, Tensor, Tensor, Tensor):
+        """
+        Decode, given an encoded source sentence.
+
+        :param encoder_output: encoder states for attention computation
+        :param encoder_hidden: last encoder state for decoder initialization
+        :param src_mask: source mask, 1 at valid tokens
+        :param trg_input: target inputs
+        :param unrol_steps: number of steps to unrol the decoder for
+        :param decoder_hidden: decoder hidden state (optional)
+        :return: decoder outputs (outputs, hidden, att_probs, att_vectors)
+        """
+        return self.decoder(trg_embed=self.trg_embed(trg_input),
+                            encoder_output=encoder_output,
+                            encoder_hidden=encoder_hidden,
+                            src_mask=src_mask,
+                            unrol_steps=unrol_steps,
+                            hidden=decoder_hidden)
+
+    def get_loss_for_batch(self, batch: Batch, criterion: nn.Module) -> Tensor:
+        """
+        Compute non-normalized loss and number of tokens for a batch
+
+        :param batch: batch to compute loss for
+        :param criterion: loss criterion
+        :return: batch_loss: sum of losses over non-pad elements in the batch
+        """
+        #pylint: disable=unused-variable
+        out, hidden, att_probs, _ = self.forward(
+            src=batch.src, trg_input=batch.trg_input,
+            src_mask=batch.src_mask, src_lengths=batch.src_lengths)
+
+        # compute log probs
+        log_probs = F.log_softmax(out, dim=-1)
+
+        # compute batch loss
+        batch_loss = criterion(
+            input=log_probs.contiguous().view(-1, log_probs.size(-1)),
+            target=batch.trg.contiguous().view(-1))
+        # return batch loss = sum over all elements in batch that are not pad
+        return batch_loss
+
+    def run_batch(self, batch: Batch, max_output_length: int, beam_size: int,
+                  beam_alpha: float) -> (np.array, np.array):
+        """
+        Get outputs and attentions scores for a given batch
+
+        :param batch: batch to generate hypotheses for
+        :param max_output_length: maximum length of hypotheses
+        :param beam_size: size of the beam for beam search, if 0 use greedy
+        :param beam_alpha: alpha value for beam search
+        :return: stacked_output: hypotheses for batch,
+            stacked_attention_scores: attention scores for batch
+        """
+        encoder_output, encoder_hidden = self.encode(
+            batch.src, batch.src_lengths,
+            batch.src_mask)
+
+        # if maximum output length is not globally specified, adapt to src len
+        if max_output_length is None:
+            max_output_length = int(max(batch.src_lengths.cpu().numpy()) * 1.5)
+
+        # greedy decoding
+        if beam_size == 0:
+            stacked_output, stacked_attention_scores = greedy(
+                encoder_hidden=encoder_hidden, encoder_output=encoder_output,
+                src_mask=batch.src_mask, embed=self.trg_embed,
+                bos_index=self.bos_index, decoder=self.decoder,
+                max_output_length=max_output_length)
+            # batch, time, max_src_length
+        else:  # beam size
+            stacked_output, stacked_attention_scores = \
+                beam_search(size=beam_size, encoder_output=encoder_output,
+                            encoder_hidden=encoder_hidden,
+                            src_mask=batch.src_mask, embed=self.trg_embed,
+                            max_output_length=max_output_length,
+                            alpha=beam_alpha, eos_index=self.eos_index,
+                            pad_index=self.pad_index, bos_index=self.bos_index,
+                            decoder=self.decoder)
+
+        return stacked_output, stacked_attention_scores
+
+    def __repr__(self) -> str:
+        """
+        String representation: a description of encoder, decoder and embeddings
+
+        :return: string representation
+        """
+        return "%s(\n" \
+               "\tencoder=%r,\n" \
+               "\tdecoder=%r,\n" \
+               "\tsrc_embed=%r,\n" \
+               "\ttrg_embed=%r)" % (
+                   self.__class__.__name__, str(self.encoder),
+                   str(self.decoder),
+                   self.src_embed, self.trg_embed)
+
+    def log_parameters_list(self, logging_function: Callable[[str], None]) \
+            -> None:
+        """
+        Write all parameters (name, shape) to the log.
+
+        :param logging_function: a logger's logging function
+        """
+        model_parameters = filter(lambda p: p.requires_grad, self.parameters())
+        n_params = sum([np.prod(p.size()) for p in model_parameters])
+        logging_function("Total params: %d" % n_params)
+        for name, p in self.named_parameters():
+            if p.requires_grad:
+                logging_function("%s : %s" % (name, list(p.size())))
+
+
 def build_model(cfg: dict = None,
                 src_vocab: Vocabulary = None,
-                trg_vocab: Vocabulary = None):
+                trg_vocab: Vocabulary = None) -> Model:
+    """
+    Build and initialize the model according to the configuration.
+
+    :param cfg: dictionary configuration containing model specifications
+    :param src_vocab: source vocabulary
+    :param trg_vocab: target vocabulary
+    :return: built and initialized model
+    """
     src_padding_idx = src_vocab.stoi[PAD_TOKEN]
     trg_padding_idx = trg_vocab.stoi[PAD_TOKEN]
 
@@ -54,187 +247,3 @@ def build_model(cfg: dict = None,
 
     return model
 
-
-class Model(nn.Module):
-    """
-    Base Model class
-    """
-
-    def __init__(self,
-                 name: str = "my_model",
-                 encoder: Encoder = None,
-                 decoder: Decoder = None,
-                 src_embed: Embeddings = None,
-                 trg_embed: Embeddings = None,
-                 src_vocab: Vocabulary = None,
-                 trg_vocab: Vocabulary = None):
-        """
-        Create a new encoder-decoder model
-
-        :param name:
-        :param encoder:
-        :param decoder:
-        :param src_embed:
-        :param trg_embed:
-        :param src_vocab:
-        :param trg_vocab:
-        """
-        super(Model, self).__init__()
-
-        self.name = name
-        self.src_embed = src_embed
-        self.trg_embed = trg_embed
-        self.encoder = encoder
-        self.decoder = decoder
-        self.src_vocab = src_vocab
-        self.trg_vocab = trg_vocab
-        self.bos_index = self.trg_vocab.stoi[BOS_TOKEN]
-        self.pad_index = self.trg_vocab.stoi[PAD_TOKEN]
-        self.eos_index = self.trg_vocab.stoi[EOS_TOKEN]
-
-    #pylint: disable=arguments-differ
-    def forward(self, src: Tensor, trg_input: Tensor, src_mask: Tensor,
-                src_lengths: Tensor):
-        """
-        Take in and process masked src and target sequences.
-        Use the encoder hidden state to initialize the decoder
-        The encoder outputs are used for attention
-
-        :param src:
-        :param trg_input:
-        :param src_mask:
-        :param src_lengths:
-        :return: decoder outputs
-        """
-        encoder_output, encoder_hidden = self.encode(src=src,
-                                                     src_length=src_lengths,
-                                                     src_mask=src_mask)
-        unrol_steps = trg_input.size(1)
-        return self.decode(encoder_output=encoder_output,
-                           encoder_hidden=encoder_hidden,
-                           src_mask=src_mask, trg_input=trg_input,
-                           unrol_steps=unrol_steps)
-
-    def encode(self, src: Tensor, src_length: Tensor, src_mask: Tensor):
-        """
-        Encodes the source sentence.
-        TODO adapt to transformer
-
-        :param src:
-        :param src_length:
-        :param src_mask:
-        :return:
-        """
-        return self.encoder(self.src_embed(src), src_length, src_mask)
-
-    def decode(self, encoder_output: Tensor, encoder_hidden: Tensor,
-               src_mask: Tensor, trg_input: Tensor,
-               unrol_steps: int, decoder_hidden: Tensor = None):
-        """
-        Decode, given an encoded source sentence.
-
-        :param encoder_output:
-        :param encoder_hidden:
-        :param src_mask:
-        :param trg_input:
-        :param unrol_steps:
-        :param decoder_hidden:
-        :return: decoder outputs
-        """
-        return self.decoder(trg_embed=self.trg_embed(trg_input),
-                            encoder_output=encoder_output,
-                            encoder_hidden=encoder_hidden,
-                            src_mask=src_mask,
-                            unrol_steps=unrol_steps,
-                            hidden=decoder_hidden)
-
-    def get_loss_for_batch(self, batch: Batch, criterion: nn.Module):
-        """
-        Compute non-normalized loss and number of tokens for a batch
-
-        :param batch:
-        :param criterion:
-        :return:
-        """
-        #pylint: disable=unused-variable
-        out, hidden, att_probs, _ = self.forward(
-            src=batch.src, trg_input=batch.trg_input,
-            src_mask=batch.src_mask, src_lengths=batch.src_lengths)
-
-        # compute log probs
-        log_probs = F.log_softmax(out, dim=-1)
-
-        # compute batch loss
-        batch_loss = criterion(
-            input=log_probs.contiguous().view(-1, log_probs.size(-1)),
-            target=batch.trg.contiguous().view(-1))
-        # return batch loss = sum over all elements in batch that are not pad
-        return batch_loss
-
-    def run_batch(self, batch: Batch, max_output_length: int, beam_size: int,
-                  beam_alpha: float):
-        """
-        Get outputs and attentions scores for a given batch
-
-        :param batch:
-        :param max_output_length:
-        :param beam_size:
-        :param beam_alpha:
-        :return:
-        """
-        encoder_output, encoder_hidden = self.encode(
-            batch.src, batch.src_lengths,
-            batch.src_mask)
-
-        # if maximum output length is not globally specified, adapt to src len
-        if max_output_length is None:
-            max_output_length = int(max(batch.src_lengths.cpu().numpy()) * 1.5)
-
-        # greedy decoding
-        if beam_size == 0:
-            stacked_output, stacked_attention_scores = greedy(
-                encoder_hidden=encoder_hidden, encoder_output=encoder_output,
-                src_mask=batch.src_mask, embed=self.trg_embed,
-                bos_index=self.bos_index, decoder=self.decoder,
-                max_output_length=max_output_length)
-            # batch, time, max_src_length
-        else:  # beam size
-            stacked_output, stacked_attention_scores = \
-                beam_search(size=beam_size, encoder_output=encoder_output,
-                            encoder_hidden=encoder_hidden,
-                            src_mask=batch.src_mask, embed=self.trg_embed,
-                            max_output_length=max_output_length,
-                            alpha=beam_alpha, eos_index=self.eos_index,
-                            pad_index=self.pad_index, bos_index=self.bos_index,
-                            decoder=self.decoder)
-
-        return stacked_output, stacked_attention_scores
-
-    def __repr__(self):
-        """
-        String representation: a description of encoder, decoder and embeddings
-
-        :return:
-        """
-        return "%s(\n" \
-               "\tencoder=%r,\n" \
-               "\tdecoder=%r,\n" \
-               "\tsrc_embed=%r,\n" \
-               "\ttrg_embed=%r)" % (
-                   self.__class__.__name__, str(self.encoder),
-                   str(self.decoder),
-                   self.src_embed, self.trg_embed)
-
-    def log_parameters_list(self, logging_function: Callable[[str], None]):
-        """
-        Write all parameters (name, shape) to the log.
-
-        :param logging_function:
-        :return:
-        """
-        model_parameters = filter(lambda p: p.requires_grad, self.parameters())
-        n_params = sum([np.prod(p.size()) for p in model_parameters])
-        logging_function("Total params: %d" % n_params)
-        for name, p in self.named_parameters():
-            if p.requires_grad:
-                logging_function("%s : %s" % (name, list(p.size())))
