50c0c2b6eeb0bbc630d3149e6dc52193efffdb7d
Julia Kreutzer <juliakreutzer1@aol.com>
2019-03-13 18:05:35 +0100

    adjust data config passing

--- joeynmt/prediction.py
+++ joeynmt/prediction.py
@@ -183,7 +183,8 @@ def test(cfg_file,
     max_output_length = cfg["training"].get("max_output_length", None)
 
     # load the data
-    _, dev_data, test_data, src_vocab, trg_vocab = load_data(cfg=cfg)
+    _, dev_data, test_data, src_vocab, trg_vocab = load_data(
+        data_cfg=cfg["data"])
 
     data_to_predict = {"dev": dev_data, "test": test_data}
 
