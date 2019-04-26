7e9c77fd1d3e48e1f98f1173c265d0315181aed0
Joost Bastings <bastings@users.noreply.github.com>
2019-04-26 22:38:13 +0200

    add label smoothing test

--- test/unit/test_transformer_utils.py
+++ test/unit/test_transformer_utils.py
@@ -1,6 +1,6 @@
 import torch
 
-from joeynmt.transformer import PositionalEncoding
+from joeynmt.transformer import PositionalEncoding, LabelSmoothing
 from .test_helpers import TensorTestCase
 
 
@@ -21,4 +21,28 @@ class TestTransformerUtils(TensorTestCase):
         output = pe(x)
         self.assertEqual(pe.pe.size(2), hidden_size)
         self.assertTensorAlmostEqual(output, pe.pe[:, :x.size(1)])
+
+    def test_label_smoothing(self):
+
+        size = 5
+        padding_idx = 0
+        smoothing = 0.4
+        criterion = LabelSmoothing(size, padding_idx, smoothing)
+
+        predict = torch.FloatTensor([[0.1, 0.1, 0.6, 0.1, 0.1],
+                                     [0.1, 0.1, 0.6, 0.1, 0.1],
+                                     [0.1, 0.1, 0.6, 0.1, 0.1]])
+
+        v = criterion(predict.log(), torch.LongTensor([2, 1, 0]))
+
+        self.assertTensorAlmostEqual(
+            criterion.true_dist,
+            torch.Tensor(
+                [[0.0000, 0.1333, 0.6000, 0.1333, 0.1333],
+                 [0.0000, 0.6000, 0.1333, 0.1333, 0.1333],
+                 [0.0000, 0.0000, 0.0000, 0.0000, 0.0000]]
+            )
+        )
+
+        self.assertAlmostEqual(v.item(), 1.0663001537)
 
