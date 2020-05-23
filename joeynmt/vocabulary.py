b3e19f802c5c673f9ff5701d709677a8567bed64
joeynmt <44170216+joeynmt@users.noreply.github.com>
2020-05-23 05:48:40 +0200

    Update vocabulary.py
    skip PAD tokens

--- joeynmt/vocabulary.py
+++ joeynmt/vocabulary.py
@@ -101,13 +101,15 @@ class Vocabulary:
     def __len__(self) -> int:
         return len(self.itos)
 
-    def array_to_sentence(self, array: np.array, cut_at_eos=True) -> List[str]:
+    def array_to_sentence(self, array: np.array, cut_at_eos=True,
+                          skip_pad=True) -> List[str]:
         """
         Converts an array of IDs to a sentence, optionally cutting the result
         off at the end-of-sequence token.
 
         :param array: 1D array containing indices
         :param cut_at_eos: cut the decoded sentences at the first <eos>
+        :param skip_pad: skip generated <pad> tokens
         :return: list of strings (tokens)
         """
         sentence = []
@@ -115,23 +117,27 @@ class Vocabulary:
             s = self.itos[i]
             if cut_at_eos and s == EOS_TOKEN:
                 break
+            elif skip_pad and s == PAD_TOKEN:
+                continue
             sentence.append(s)
         return sentence
 
-    def arrays_to_sentences(self, arrays: np.array, cut_at_eos=True) \
-            -> List[List[str]]:
+    def arrays_to_sentences(self, arrays: np.array, cut_at_eos=True,
+                            skip_pad=True) -> List[List[str]]:
         """
         Convert multiple arrays containing sequences of token IDs to their
         sentences, optionally cutting them off at the end-of-sequence token.
 
         :param arrays: 2D array containing indices
         :param cut_at_eos: cut the decoded sentences at the first <eos>
+        :param skip_pad: skip generated <pad> tokens
         :return: list of list of strings (tokens)
         """
         sentences = []
         for array in arrays:
             sentences.append(
-                self.array_to_sentence(array=array, cut_at_eos=cut_at_eos))
+                self.array_to_sentence(array=array, cut_at_eos=cut_at_eos,
+                                       skip_pad=skip_pad))
         return sentences
 
 
