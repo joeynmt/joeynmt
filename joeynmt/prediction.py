48dc07ee8f31fc94d33e61f7483f03a5f8d77ebd
Catherine <catherinegitau94@gmail.com>
2020-09-04 11:36:07 +0000

    fixed indentation issue

--- joeynmt/prediction.py
+++ joeynmt/prediction.py
@@ -158,10 +158,9 @@ def validate_on_data(model: Model, data: Dataset,
             elif eval_metric.lower() == 'sequence_accuracy':
                 current_valid_score = sequence_accuracy(
                     valid_hypotheses, valid_references)
-	    elif eval_metric.lower() == 'meteor':
-		current_valid_score = meteor(
+            elif eval_metric.lower() == 'meteor':
+                current_valid_score = meteor(
                     valid_hypotheses, valid_references)
-		
         else:
             current_valid_score = -1
 
