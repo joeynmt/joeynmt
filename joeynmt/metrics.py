75806e087d139f83e1b19e60a89eb71ade8b7e3f
joeynmt <44170216+joeynmt@users.noreply.github.com>
2020-10-15 16:46:34 +0200

    Update metrics.py

--- joeynmt/metrics.py
+++ joeynmt/metrics.py
@@ -14,7 +14,8 @@ def chrf(hypotheses, references):
     :param references: list of references (strings)
     :return:
     """
-    return sacrebleu.corpus_chrf(hypotheses=hypotheses, references=[references]).score
+    return sacrebleu.corpus_chrf(
+        hypotheses=hypotheses, references=[references]).score
 
 
 def bleu(hypotheses, references):
