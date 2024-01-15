be9087882102694ef7fbc03845a70cd47a3b42dc
Mayumi Ohta <ohta@cl.uni-heidelberg.de>
2024-01-15 13:37:55 +0100

    refactor generate_reverse_task.py

--- scripts/generate_reverse_task.py
+++ scripts/generate_reverse_task.py
@@ -28,37 +28,40 @@ def save_samples(samples, filename, reverse=False):
 
 
 def generate_task(args):
+    out_dir = Path(args.out_dir)
+    out_dir.mkdir(exist_ok = True)
 
     # train
     samples = generate_samples(n=args.train_size, low=args.low, high=args.high, maxlen=args.maxlen)
-    filename = Path(args.out_dir) / "train"
+    filename = out_dir / "train"
     save_samples(samples, filename.with_suffix('.src'), reverse=False)
     save_samples(samples, filename.with_suffix('.trg'), reverse=True)
 
     # dev
     samples = generate_samples(n=args.dev_size, low=args.low, high=args.high, maxlen=args.maxlen+5)
-    filename = Path(args.out_dir) / "dev"
+    filename = out_dir / "dev"
     save_samples(samples, filename.with_suffix('.src'), reverse=False)
     save_samples(samples, filename.with_suffix('.trg'), reverse=True)
 
     # test
     samples = generate_samples(n=args.test_size, low=args.low, high=args.high, maxlen=args.maxlen+5)
-    filename = Path(args.out_dir) / "test"
+    filename = out_dir / "test"
     save_samples(samples, filename.with_suffix('.src'), reverse=False)
     save_samples(samples, filename.with_suffix('.trg'), reverse=True)
 
 
 if __name__ == "__main__":
-    PATH = os.path.dirname(os.path.abspath("__file__"))
+    root_path = Path(__file__).parent.resolve()
 
     ap = argparse.ArgumentParser("Generate data for the reverse task")
     ap.add_argument(
         "--out_dir",
         type=str,
-        default=os.path.join(PATH, "../test/data/reverse"),
+        default=(root_path / "../test/data/reverse").as_posix(),
         help="path to output dir. default: ../test/data/reverse",
     )
     ap.add_argument("--train_size", type=int, default=50000, help="train set size")
+    ap.add_argument("--dev_size", type=int, default=1000, help="dev set size")
     ap.add_argument("--test_size", type=int, default=1000, help="test set size")
     ap.add_argument("--low", type=int, default=0, help="min value")
     ap.add_argument("--high", type=int, default=50, help="max value")
